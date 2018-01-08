1bb614daafaf509655f5c35500e389109761db21
Roger <roger.e.clayton@gmail.com>
2018-01-08 12:27:59 -0800

    bulma imported and routed through variable file

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <section class="container">
     <div>
       <logo/>
-      <h1 class="title">
+      <h1 class="title has-text-primary">
         kristinsongs
       </h1>
       <h2 class="subtitle">
