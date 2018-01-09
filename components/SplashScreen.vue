82c196c91e6481a683db75ea141440a8b42f3d58
Roger <roger.e.clayton@gmail.com>
2018-01-08 16:19:02 -0800

    removes default content - adds spashscreen component

---
+++ components/SplashScreen.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="hero is-primary">
+    <div class="hero-body">
+      <p class="title">testing</p>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+
+    name: 'SplashScreen',
+
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style lang="sass" scoped>
+</style>
