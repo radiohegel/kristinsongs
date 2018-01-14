61f1d3a998d053f12958ba6e58f6bff00e0418d6
Roger <roger.e.clayton@gmail.com>
2018-01-13 20:52:57 -0800

    adds navbar draft with placeholder cdbaby player

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
+		<nav class="navbar">
+			<div class="navbar-brand">
+				<p class="navbar-item">KristinSongs</p>
+			</div>
+			<div class="navbar-menu">
+				<a class="navbar-item"> About</a>
+				<a class="navbar-item"> Store</a>
+				<a class="navbar-item"> Contact</a>
+			</div>
+			<div class="navbar-menu navbar-end">
+				<span class="navbar-item">
+					<iframe name="mini" style="border:0px;width:100%;height:100px;" src="//widget.cdbaby.com//mini/light/opaque"></iframe>
+				</span>
+			</div>
+		</nav>
     <nuxt/>
   </div>
 </template>
