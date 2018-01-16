384229aa3e917fb0ae073b0fd023df696d866afe
Roger <roger.e.clayton@gmail.com>
2018-01-16 11:33:14 -0800

    adds real player with full album

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,7 +11,7 @@
 			</div>
 			<div class="navbar-menu navbar-end">
 				<span class="navbar-item">
-					<iframe name="mini" style="border:0px;width:100%;height:75px;" src="//widget.cdbaby.com/mini/light/opaque"></iframe>
+					<iframe name="mini" style="border:0px;width:100%;height:70px;" src="//widget.cdbaby.com/60cd6bd3-716f-4d90-80d7-4d7b5c458146/mini/light/transparent"></iframe>
 				</span>
 			</div>
 		</nav>
