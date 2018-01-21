6834f3f6ee4a02db0f299a739d714d923c2db83c
Roger <roger.e.clayton@gmail.com>
2018-01-21 12:48:17 -0800

    removes about-section from splash page

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,14 @@
 <template>
 	<div>
 	  <splash-screen/>
-	  <about-section/>
 	</div>
 </template>
 
 <script>
   import SplashScreen from '../components/SplashScreen.vue'
-  import AboutSection from '../components/AboutSection.vue'
 
   export default {
-    components: { SplashScreen, AboutSection }
+    components: { SplashScreen }
   }
 </script>
 
