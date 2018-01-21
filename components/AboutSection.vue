e45493e2a722b6532c212213c8d02124add212ee
Roger <roger.e.clayton@gmail.com>
2018-01-21 10:07:34 -0800

    adds aboutsection component

---
+++ components/AboutSection.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+	export default {
+
+	  name: 'AboutSection',
+
+	  data () {
+	    return {
+
+	    }
+	  }
+}
+</script>
+
+<style lang="sass" scoped>
+</style>
