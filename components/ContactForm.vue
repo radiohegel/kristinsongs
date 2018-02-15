772747d81aa44d73fffe10d356f0a675ff3a9760
Roger <roger.e.clayton@gmail.com>
2018-02-15 13:19:32 -0800

    completes form layout

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,9 +1,38 @@
 <template>
 	<div class="form-collection">
 		<div class="field">
-			<label for="" class="label">name</label>
-			<div class="control"><input type="text" class="input" placeholder="Type your name here"></div>
+			<label for="" class="label">Name</label>
+			<div class="control">
+				<input type="text" class="input" placeholder="Type your name here">
+			</div>
 		</div>
+		<div class="field">
+			<label for="" class="label">Email</label>
+			<div class="control">
+				<input type="text" class="input" placeholder="Type your Email address here">
+			</div>
+		</div>		
+		<div class="field">
+			<label for="" class="label">Subject</label>
+			<div class="control">
+				<input type="text" class="input" placeholder="Type your message subject here">
+			</div>
+		</div>
+		<div class="field">
+			<label for="" class="label">Subject</label>
+			<div class="control">
+				<textarea type="text" class="textarea" placeholder="Type your message here"></textarea>
+			</div>
+		</div>
+		<div class="field-body">
+	    <div class="field">
+	      <div class="control">
+	        <button class="button is-primary">
+	          Send message
+	        </button>
+	      </div>
+	    </div>
+  	</div>
 	</div>
 </template>
 
