f4f79bacd187cffafe9dc74cb6a1cd7838a61d96
Roger <roger.e.clayton@gmail.com>
2018-02-15 12:55:00 -0800

    adds vue component skeleton for contact form

---
+++ components/ContactForm.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+	export default {
+
+	  name: 'ContactForm',
+
+	  data () {
+	    return {
+
+	    }
+	  }
+	}
+</script>
+
+<style lang="sass" scoped>
+</style>
